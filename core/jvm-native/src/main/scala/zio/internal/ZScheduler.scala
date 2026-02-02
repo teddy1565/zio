af6b7a45c95d2873ed7ba1ecee2a66a0e1a66f6f
Teddy Xiong <teddy1565@gmail.com>
2026-02-02 18:49:21 +0800

    feat: 🎸 simple test for nio

--- core/jvm-native/src/main/scala/zio/internal/ZScheduler.scala
+++ core/jvm-native/src/main/scala/zio/internal/ZScheduler.scala
@@ -25,6 +25,8 @@ import java.util.concurrent.{ConcurrentLinkedQueue, ThreadLocalRandom}
 import scala.collection.mutable
 import scala.concurrent.{BlockContext, CanAwait}
 
+import java.util.concurrent.ThreadLocalRandom
+
 /**
  * A `ZScheduler` is an `Executor` that is optimized for running ZIO
  * applications. Inspired by "Making the Tokio Scheduler 10X Faster" by Carl
@@ -148,11 +150,29 @@ private final class ZScheduler(autoBlocking: Boolean) extends Executor { parent 
     if (isBlocking(worker, runnable)) {
       submitBlocking(runnable)
     } else {
-      if ((worker eq null) || worker.blocking) {
+      var submitted = false
+      if ((worker ne null) && !worker.blocking) {
+        val random = ThreadLocalRandom.current()
+        val index = random.nextInt(poolSize)
+        val other = workers(index)
+        if ((other ne worker) && other.localQueue.size() < worker.localQueue.size()) {
+          if (ohter.localQueue.offer(runnable)) {
+            submitted = true
+          }
+        }
+
+        if (!submitted) {
+          if (!worker.localQueue.offer(runnable)) {
+            handleFullWorkerQueue(worker, runnable)
+          }
+          submitted = true
+        }
+
+      } else {
         globalQueue.offer(runnable)
-      } else if (!worker.localQueue.offer(runnable)) {
-        handleFullWorkerQueue(worker, runnable)
-      } else ()
+        submitted = true
+      }
+
       val currentState = state.get
       maybeUnparkWorker(currentState)
       true
